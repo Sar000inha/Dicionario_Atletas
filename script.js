2c98eb0f3d37c26a786cb1d9006ec91b3d3a3bd3
Sarah Cabral <sarah000cabral@gmail.com>
2022-12-29 18:53:55 -0300

    2 commit - Previa imcompleta

---
+++ script.js
@@ -0,0 +1,58 @@
+function dadosAtletas(){
+  var dados = {
+    d1 : {
+      nome: "Yuzuru Hanyu",
+      imagem: "img/yuzuru_hanyu.jpg", 
+      descricao: "Com apenas 19 anos, Hanyu se tornou o primeiro asiático a ganhar uma medalha de ouro olímpica quando triunfou em Sochi, tornando-se o mais jovem campeão masculino desde 1948. Ele seguiu defendendo seu título olímpico em PyeongChang 2018, tornando-se o primeiro homem a voltar atrás para o ouro olímpico desde 1952 (Richard Button). Durante sua carreira, Hanyu também ganhou dois títulos mundiais e quatro Grand Prix Finals - consolidando-se como talvez um dos melhores atletas de gelo do Japão, e também seu lugar como um dos maiores patinadores artísticos da história. Hanyu também foi o primeiro skatista a conseguir com sucesso um salto de loop quádruplo na competição da ISU, fazendo isso em 2016. em 19 de julho de 2022, o atleta anunciou sua decisão de se tornar profissional e 'se afastar' das patinação artística competitiva após uma carreira sênior de 12 anos, que de acordo com o Nikkei Asia, 'marca o fim de uma era'.",
+      citacao: "Tento encontrar desafios depois de cada competição e superá-los da próxima vez."},
+    d2 : {
+      nome: "Larissa Latynina", 
+      imagem: "img/larissa_latynina.jpg", 
+      descricao: "Com apenas 21 anos, Latynina fez sua estreia olímpica nos Jogos de Melbourne em 1956. Ela lutou contra uma concorrência feroz para ganhar o ouro no evento all-around. Nas finais de aparelhos, ela terminou em primeiro no salto, em segundo nas barras irregulares e no exercício de chão, e em quarto na trave de equilíbrio. Ela também levou a União Soviética à vitória no evento por equipes. Nos Jogos de Roma de 1960, Latynina defendeu seu título geral e ganhou outra medalha de ouro no evento por equipes. Ela também ganhou uma segunda medalha de ouro no exercício de solo, ganhou medalhas de prata nas barras irregulares e na trave de equilíbrio, e uma medalha de bronze no salto. No entanto, apesar de ganhar sete medalhas de ouro olímpicas, sua fome de sucesso não diminuiu. Quatro anos depois, Latynina ganhou sua terceira medalha de ouro olímpica por equipes e uma medalha de prata no evento all-around. Nas finais do aparelho, ela ganhou uma medalha de prata no salto, medalhas de bronze nas barras irregulares e na trave de equilíbrio, e venceu o exercício de solo pela terceira vez consecutiva. As incríveis conquistas de Latynina fazem dela a detentora de vários recordes olímpicos. Até Michael Phelps quebrar seu recorde em Londres, ela era a única atleta em qualquer esporte a ter ganhado dezoito medalhas olímpicas. Ela é uma das quatro únicas atletas a ganhar nove medalhas de ouro e uma das três únicas mulheres a ter vencido o mesmo evento nos Jogos de Verão três vezes.", 
+      citacao: "Quarenta e oito anos é quase tempo suficiente para manter um recorde."},
+    d3 : {
+      nome: "Usain Bolt", 
+      imagem: "img/usain_bolt.jpg", 
+      descricao: "Bolt nasceu em 21 de agosto de 1986 em Sherwood Content, Jamaica. Ele cresceu como um garoto obcecado por esportes, destacando-se no futebol e no críquete. No ensino médio, seu treinador de críquete o encorajou a tentar o atletismo seriamente. Foi uma boa decisão, aos 16 anos, em 2002, foi campeão mundial júnior. Em 2007, ele era o homem mais rápido da história, estabelecendo seu primeiro recorde mundial de 100m, e um ano depois, nos Jogos Olímpicos de Pequim 2008, ele subiu para o estrelato internacional. Na China, Bolt triunfou nos 100m rasos masculinos e, em seguida, acrescentou o título de 200m e 4x100m para garantir um lendário triplo. Ele quebrou o recorde mundial em todos os três eventos. Para vencer a prova da costela azul, os 100m, apenas uma vez, garantem a imortalidade olímpica. Fazê-lo três vezes em Jogos consecutivos, e adicionar os títulos de revezamento 200m e 4x100m à mistura, dá-lhe um bom caso para ser considerado o maior atleta de todos os tempos.", citacao: "Parei de me preocupar com a largada. O fim é o que é importante."
+      },
+    d4 : {
+      nome: "Fabiana Alvim", 
+      imagem: "img/fabiana_alvim.jpg", 
+      descricao: "Fabiana ou mais conhecida como Fabi nasceu na cidade do Rio de Janeiro no ano de 1980. Fabiana começou a jogar vôlei aos treze anos de idade na sua escola quando estava participando de uma competição escolar. Começou a treinar nas categorias de base, e devido sua baixa estatura se especializou na defesa, e logo com a criação da posição de líbero em 1998, passou a se destacar. Participou das principais conquistas da Seleção Brasileira, incluindo as medalhas de ouro nos Jogos Olímpicos de Verão de 2008 em Pequim e nos Jogos Olímpicos de Verão de 2012 em Londres. Fabiana é considerada por muitos críticos do voleibol como a melhor líbero do mundo da história. Após vinte anos de carreira, em 2018, Fabiana anunciou sua aposentadoria do esporte; atualmente é comentarista esportiva na nos canais de esporte da Globo.", 
+      citacao: "Se dedicar muito. Acho que com muito empenho e amor, independentemente do que aconteça, faz com que tenhamos orgulho da nossa profissão."},
+    d5 : {
+      nome: "Michael Phelps", 
+      imagem: "img/michael_phelps.jpg", 
+      descricao: "Phelps nasceu em Baltimore em 30 de junho de 1985 e ficou sério com o esporte depois de se juntar ao North Baltimore Aquatic Club. Ele foi criado pela mãe Deborah, ao lado de suas irmãs Hilary e Whitney, depois que seus pais se divorciaram. Phelps tem 28 medalhas no total: suas 23 medalhas de ouro são mais do que o dobro da contagem de seus rivais mais próximos, e também não é como se outros nadadores tivessem acumulado números bobos de medalhas. Além de Spitz, Matt Biondi (EUA, oito ouros) e Jenny Thompson (EUA, oito ouros), nenhum outro nadador em todo o mundo conseguiu mais de seis medalhas de ouro no total. 23 é espantoso. Michael Phelps é indiscutivelmente o maior atleta olímpico de todos os tempos. Alguns podem argumentar que Usain Bolt, Carl Lewis ou Nadia Comaneci têm uma reivindicação. Mas para o grande número de medalhas reivindicadas, há um vencedor claro e sem dúvida: Michael Phelps.", 
+      citacao: "Você não pode colocar um limite em nada. Quanto mais você sonha, mais longe você fica."},
+    d6 : {
+      nome: "Miraildes Maciel Mota", 
+      imagem: "img/formiga.jpg", 
+      descricao: "Miraildes Maciel Mota nasceu em Salvador e é a única menina dentre os cincos filhos de sua mãe. Desde pequena, adorava procurar objetos redondos para chutar por aí. Aos 7 anos, os familiares já sabiam que o talento para o futebol começava a aflorar. Com mais de 200 jogos pela seleção brasileira, Miraildes Maciel Mota, mais conhecida como Formiga, é uma das maiores atletas da história do futebol do país e carrega vários recordes. Em 2019, se tornou a primeira jogadora a atuar em sete Copas do Mundo, um recorde histórico - foi convocada em 1995, 1999, 2003, 2007, 2011, 2015 e 2019. Além disso, é a única jogadora de futebol a participar de seis edições das Olimpíadas - jogou em 1996, 2000, 2004, 2008, 2012 e 2016. É, também, a jogadora mais velha a marcar um gol na Copa do Mundo feminina, a mais velha a participar de uma edição da competição, e a atleta com mais jogos pela seleção brasileira, tendo ultrapassado Cafu em 2019. Aos 44 anos, Formiga segue atuando pelo São Paulo, clube onde começou a carreira ao chegar de Salvador, sua cidade natal. Também estreia como comentarista da Copa do Mundo 2022 na Globo.", 
+      citacao: "Mas mesmo assim eu continuava. Eu apanhava. No outro dia eu ia lá e jogava de novo, apanhava, engolia o choro e voltava a jogar"},
+  }
+
+  var cardAtletas = document.getElementById("cardAtletas");
+
+  for(var i in dados){
+    cardAtletas.innerHTML += '<div class="card">' + '<img src="'+dados[i].imagem+ '"/>' + '<details>' + '<summary>'+dados[i].nome+"</summary>" + "<p>" +dados[i].descricao + "</p>"  + "<blockquote>" + "<q>"+ dados[i].citacao + "</q>" + "</blockquote>" + "</details>" + "</div>";
+  }
+}
+
+function recomendacoes(){
+  var atletas = {
+    n1 : {nome: "Rebeca Andrade", link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"},
+    n2 : {nome: "Nathan Chen", link: "https://en.wikipedia.org/wiki/Nathan_Chen"}, 
+    n3 : {nome: "Sky Brown", link: "https://en.wikipedia.org/wiki/Sky_Brown"},
+    n4 : {nome: "Kylian Mbappé", link: "https://pt.wikipedia.org/wiki/Kylian_Mbapp%C3%A9"}, 
+  }
+  
+  var listaAtletas = document.getElementById("listaAtletas");
+
+  for(var j in atletas){
+      listaAtletas.innerHTML += '<li>' + '<a target="_blank" href="' + atletas[j].link + '">' + atletas[j].nome + '</a>' + '</li>';
+  } 
+}
+
+dadosAtletas();
+recomendacoes();
